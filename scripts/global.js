70061a8af635277c23162c4990eda74c1e4dc1cb
brittanyramirez <brittanyramirez@R2Hs-MBP.lan>
2025-11-10 16:24:11 -0500

    Brittany_resourcepage

---
+++ scripts/global.js
@@ -0,0 +1,12 @@
+// Opens respective pages in a new tab
+function openIG() {
+  window.open('https://www.instagram.com', '_blank');
+}
+
+function openFB(){
+  window.open('https://www.facebook.com', '_blank');
+}
+
+function openLI (){
+  window.open('https://www.linkedin.com', '_blank');
+}
